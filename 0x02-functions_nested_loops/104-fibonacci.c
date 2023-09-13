1e18f730666038f8c396bbd027097353c7ec7e92
stanley <pookers101@hotmail.com>
2023-09-13 12:59:09 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,7 +35,7 @@ int main(void)
 	half2 %= 10000000000;
 	}
 	printf("%lu%lu", half1, half2);
-	if (count !=98)
+	if (count != 98)
 	printf(", ");
 	fib1_half1 = fib2_half1;
 	fib1_half2 = fib2_half2;
